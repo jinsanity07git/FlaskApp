808e56a0ca299642d4111c5fa9c1c6ff33d88bb7
jinsanity07git <443605310@qq.com>
2020-04-20 23:06:07 -0500

    by 4-10 ORM与CodeFirst区别

---
+++ app/spider/yushun_book.py
@@ -0,0 +1,23 @@
+from app.libs.Nhttp import HTTP
+from flask import current_app
+
+class YuShuBook:
+    isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
+    keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
+
+    @classmethod
+    def search_by_isbn(cls, isbn):
+        url = cls.isbn_url.format(isbn)
+        result = HTTP.get(url)
+        return result
+
+    @classmethod
+    def search_by_keyword(cls, keyword, page=1):
+        url = cls.keyword_url.format(keyword, current_app.config['PER_PAGE']
+                                     , cls.calculate_start(page))
+        results = HTTP.get(url)
+        return results
+
+    @staticmethod
+    def calculate_start(page):
+        return (page - 1) * current_app.config['PER_PAGE']
